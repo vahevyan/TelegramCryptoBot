f36c79814d5d8201908e81b7060b4da212dec60b
vahevyan <vahevyann@gmail.com>
2023-04-18 20:51:47 +0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	crypto_bot.py

--- crypto_app.py
+++ crypto_app.py
@@ -1,33 +1,27 @@
 import yfinance as yf
+from matplotlib import pyplot as plt
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 
 
 def get_crypto_data(symbol):
     crypto = yf.Ticker(symbol)
     data = crypto.history(period='1d')
     price = data['Close'][0]
-    return price
-
+    return data, price
 
 class CryptoButtons(GridLayout):
     def __init__(self, **kwargs):
         super(CryptoButtons, self).__init__(**kwargs)
         self.cols = 3
         self.add_widget(Button(text='(BTC) Bitcoin', on_press=lambda x: self.show_crypto('BTC-USD')))
-        self.add_widget(Button(text='(ETH) Ethereum', on_press=lambda x: self.show_crypto('ETH-USD')))
-        self.add_widget(Button(text='(LTC) Litecoin', on_press=lambda x: self.show_crypto('LTC-USD')))
-        self.add_widget(Button(text='(DOGE) Dogecoin', on_press=lambda x: self.show_crypto('DOGE-USD')))
-        self.add_widget(Button(text='(XRP) Ripple', on_press=lambda x: self.show_crypto('XRP-USD')))
-        self.add_widget(Button(text='(XLM) Stellar', on_press=lambda x: self.show_crypto('XLM-USD')))
 
     def show_crypto(self, symbol):
         self.parent.display_crypto(symbol)
-
-
 
 class CryptoDisplay(GridLayout):
     def __init__(self, **kwargs):
@@ -41,10 +35,10 @@ class CryptoDisplay(GridLayout):
         self.add_widget(self.price_label)
 
     def update_display(self, symbol):
-        price = get_crypto_data(symbol)
+        data, price = get_crypto_data(symbol)
         self.symbol_label.text = symbol
         self.price_label.text = str(price)
-
+        self.parent.graph.update_graph(data)
 
 class CryptoCalculator(GridLayout):
     def __init__(self, **kwargs):
@@ -65,6 +59,14 @@ class CryptoCalculator(GridLayout):
         total = amount * price
         self.total_label.text = str(total)
 
+class CryptoGraph(FigureCanvasKivyAgg):
+    def __init__(self, **kwargs):
+        super(CryptoGraph, self).__init__(plt.figure(**kwargs))
+        self.ax = self.figure.subplots()
+
+    def update_graph(self, data):
+        data['Close'].plot(ax=self.ax)
+        self.draw()
 
 class CryptoGrid(GridLayout):
     def __init__(self, **kwargs):
@@ -73,20 +75,20 @@ class CryptoGrid(GridLayout):
         self.buttons = CryptoButtons()
         self.display = CryptoDisplay()
         self.calculator = CryptoCalculator()
+        self.graph = CryptoGraph(figsize=(5, 3), dpi=100)
         self.add_widget(self.buttons)
         self.add_widget(self.display)
         self.add_widget(self.calculator)
+        self.add_widget(self.graph)
 
     def display_crypto(self, symbol):
         self.display.update_display(symbol)
         self.calculator.amount_input.text = ''
         self.calculator.total_label.text = ''
 
-
 class CryptoApp(App):
     def build(self):
         return CryptoGrid()
-
 
 if __name__ == '__main__':
     CryptoApp().run()
